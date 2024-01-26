01bbbd94cd2ea6b9e0b86406dd0a363d65d3b81a
Bancha-t <gmtv7887@gmail.com>
2024-01-26 15:24:14 +0700

    gg

--- lab15_4.cpp
+++ lab15_4.cpp
@@ -17,3 +17,18 @@ int main(){
 	
 	return 0;
 }
+
+void shuffle(int &he1 , int &he2, int &he3 , int &he4){
+	int payhe[] = {he1,he2,he3,he4} ;
+	he1 = payhe[rand()%4] ;
+	do{
+		he2 = payhe[rand()%4] ;
+	}while(he2 == he1) ;
+	do{
+		he3 = payhe[rand()%4] ;
+	}while(he3 == he2 || he3 == he1) ;
+	do{
+		he4 = payhe[rand()%4] ; 
+	}while(he4 == he1 || he4 == he2 || he4 == he3) ;
+
+}
--- lab15_5.cpp
+++ lab15_5.cpp
@@ -3,7 +3,7 @@
 #include <ctime>
 using namespace std;
 
-void shuffle(int &,int &, int &, int &); //Modify input arguments to pointer 
+void shuffle(int *,int *, int *, int *); 
 
 int main(){
 	int a = 50, b = 100, c = 500, d = 1000;
@@ -11,11 +11,23 @@ int main(){
 	srand(time(0));	
 	
 	for(int i = 0;i < 10;i++){
-	    shuffle(a,b,c,d); //Modify input arguments to pointer 
+	    shuffle(&a,&b,&c,&d); 
 	    cout << a << " " << b << " " << c << " " << d << "\n";
 	}
 	
 	return 0;
 }
 
-//Write definition of shuffle() using pointer here 
+void shuffle(int *he1 , int *he2, int *he3 , int *he4){
+	int payhe[] = {*he1,*he2,*he3,*he4} ;
+	*he1 = payhe[rand()%4] ;
+	do{
+		*he2 = payhe[rand()%4] ;
+	}while(*he2 == *he1) ;
+	do{
+		*he3 = payhe[rand()%4] ;
+	}while(*he3 == *he2 || *he3 == *he1) ;
+	do{
+		*he4 = payhe[rand()%4] ; 
+	}while(*he4 == *he1 || *he4 == *he2 || *he4 == *he3) ;
+}
